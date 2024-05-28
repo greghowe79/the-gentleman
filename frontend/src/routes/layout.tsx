4c192aca40e0f78078fca2bba527c92b0fc80594
Alessandro Mosca <alessandro.a.mosca@accenture.com>
2024-05-28 14:59:41 +0200

    impostato RequestHandler sul layout.tsx principale per fornire i dati aggiornati senza caching

--- frontend/src/routes/layout.tsx
+++ frontend/src/routes/layout.tsx
@@ -15,14 +15,23 @@ import {
   updateTable,
 } from '~/utils/product_detail_page_utils/actions_product_detail_page';
 
+// export const onGet: RequestHandler = async ({ cacheControl }) => {
+//   // Control caching for this request for best performance and to reduce hosting costs:
+//   // https://qwik.builder.io/docs/caching/
+//   cacheControl({
+//     // Always serve a cached response by default, up to a week stale
+//     staleWhileRevalidate: 60 * 60 * 24 * 7,
+//     // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+//     maxAge: 5,
+//   });
+// };
+
 export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.builder.io/docs/caching/
   cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
+    public: false,
+    maxAge: 0,
+    sMaxAge: 0,
+    staleWhileRevalidate: 0,
   });
 };
 
