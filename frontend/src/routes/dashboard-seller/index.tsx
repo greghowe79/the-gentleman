2a0392b3a94ea69281a01a5879c267443ed9411e
Alessandro Mosca <alessandro.a.mosca@accenture.com>
2024-06-01 17:48:03 +0200

    aggiunta immagine e modificato layout su pagina become a seller, modificate layout delle card e carousel

--- frontend/src/routes/dashboard-seller/index.tsx
+++ frontend/src/routes/dashboard-seller/index.tsx
@@ -3,9 +3,22 @@ import { UserSessionContext } from '~/root';
 import { createConnectAccount, getAccountBalance } from '~/utils/stripe';
 import { useLocation, useNavigate } from '@builder.io/qwik-city';
 import Loader from '~/components/loader/component/Loader';
-import { notConnWrap, title_h3 } from './styles.css';
+import {
+  wrap,
+  imgAccessoriesWrap,
+  notConnWrap,
+  overlay,
+  subtitle,
+  title_h3,
+  advantages_container,
+  img_accessories,
+  wrapper,
+  title,
+} from './styles.css';
 import Connected from '~/components/connected/component/connected';
 import type { BalanceProps } from '~/components/connected/types/types';
+import { Image } from '@unpic/qwik';
+import imageUrl from '~/media/accessories.jpg';
 
 const SellerPage = component$(() => {
   const balance = useSignal<BalanceProps | undefined>();
@@ -32,20 +45,41 @@ const SellerPage = component$(() => {
 
   const notConnected = $(() => {
     return (
-      <div class={notConnWrap}>
-        <h3 class={title_h3}>Setup payouts to post your products</h3>
-        <div style={{ color: 'black' }}>THE GENTLEMAN partners with stripe to transfer earning to your bank account</div>
+      <>
+        <div class={imgAccessoriesWrap}>
+          <Image
+            objectFit="cover"
+            src={imageUrl}
+            layout="constrained"
+            decoding="async"
+            loading="lazy"
+            alt={`Men's Accessories`}
+            class={img_accessories}
+          />
+          <div class={overlay}></div>
+          <div class={wrapper}>
+            <div class={wrap}>
+              <h2 class={title}>Increase Your Revenue.</h2>
+              <h3 class={subtitle}>Reach Thousands of Potential Customers for Men's Accessories Every Day</h3>
+              <div class={advantages_container}></div>
+            </div>
+          </div>
+        </div>
+        <div class={notConnWrap}>
+          <h3 class={title_h3}>Setup payouts to post your products</h3>
+          <div style={{ color: 'black' }}>THE GENTLEMAN partners with stripe to transfer earning to your bank account</div>
 
-        <button
-          disabled={loading.value}
-          onClick$={() => (userSession.isLoggedIn ? handleClick() : alert('EFFETTUA IL LOGIN'))}
-          style={{ display: loc.url.href !== 'http://localhost/dashboard-seller/' ? 'none' : 'block' }}
-        >
-          {loading.value ? 'Processing...' : 'Setup Payouts'}
-        </button>
+          <button
+            disabled={loading.value}
+            onClick$={() => (userSession.isLoggedIn ? handleClick() : alert('EFFETTUA IL LOGIN'))}
+            style={{ display: loc.url.href !== 'http://localhost/dashboard-seller/' ? 'none' : 'block' }}
+          >
+            {loading.value ? 'Processing...' : 'Setup Payouts'}
+          </button>
 
-        <div style={{ color: 'black' }}>You'll be redirect to Stripe to complete the onboarding process</div>
-      </div>
+          <div style={{ color: 'black' }}>You'll be redirect to Stripe to complete the onboarding process</div>
+        </div>
+      </>
     );
   });
 
