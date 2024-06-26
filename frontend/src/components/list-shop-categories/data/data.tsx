ef9c1673e48eacd9f4b02a317ff190a56b66bb08
Alessandro Mosca <alessandro.a.mosca@accenture.com>
2024-06-27 01:31:41 +0200

    creata nuova categoria e cambiate descrizioni nel DB

--- frontend/src/components/list-shop-categories/data/data.tsx
+++ frontend/src/components/list-shop-categories/data/data.tsx
@@ -1,8 +1,8 @@
 export const categories = [
   { id: 0, slug: 'shoes', text: 'Shoes' },
-  { id: 1, slug: 'fine-dining', text: 'Fine Dining' },
-  { id: 2, slug: 'high-end-fashion', text: 'High-End Fashion' },
-  { id: 3, slug: 'exclusive-wellness', text: 'Exclusive Wellness' },
+  { id: 1, slug: 'wearables', text: 'Wearables' },
+  { id: 2, slug: 'supplements', text: 'Supplements' },
+  { id: 3, slug: 'clothing', text: 'Clothing' },
   { id: 4, slug: 'vip-entertainment', text: 'VIP Entertainment' },
   { id: 5, slug: 'supercar-tours', text: 'Supercar Tours' },
   { id: 6, slug: 'personalized-financial-services', text: 'Personalized Financial Services' },
